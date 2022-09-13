5e8be695f613b0527ab770ac0a23b2ca85183352
jonaskaz <jonas.kazlauskas@me.com>
2022-09-13 13:57:25 -0400

    Fix teleop and bag file

--- comprobo_warmup/teleop.py
+++ comprobo_warmup/teleop.py
@@ -20,41 +20,30 @@ class TeleopPublisher(Node):
     def __init__(self):
         super().__init__("teleop")
         self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.create_timer(0.1, self.run_teleop)
-    
-    def run_teleop(self):
-        """
-        Watch for key presses and
-        Send corresponding velocities
-        """
-        settings = termios.tcgetattr(sys.stdin)
-        key = None
-        msg = Twist()
-        # TODO: Make control-c work
-        while True:
-            key = self.getKey(settings)
-            if key == '\x03':
-                return
-            if key in key_binds.keys():
-                msg.linear.x = key_binds[key][0]
-                msg.linear.y = key_binds[key][1]
-                msg.linear.z = key_binds[key][2] 
-                msg.angular.z = key_binds[key][3]
-                self.pub.publish(msg)
+        self.msg = Twist()
+        
 
-    @staticmethod
-    def getKey(settings):
-        tty.setraw(sys.stdin.fileno())
-        select.select([sys.stdin], [], [], 0)
-        key = sys.stdin.read(1)
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-        return key
+def getKey(settings):
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
 
 def main(args=None):
     rclpy.init(args=args)
-    teleop_publisher = TeleopPublisher()
-    rclpy.spin(teleop_publisher)
+    tp = TeleopPublisher()
+    settings = termios.tcgetattr(sys.stdin)
+    key = None
+    while key != '\x03':
+        key = getKey(settings)
+        if key in key_binds.keys():
+            tp.msg.linear.x = key_binds[key][0]
+            tp.msg.linear.y = key_binds[key][1]
+            tp.msg.linear.z = key_binds[key][2] 
+            tp.msg.angular.z = key_binds[key][3]
+        tp.pub.publish(tp.msg)
     rclpy.shutdown()
 
 
